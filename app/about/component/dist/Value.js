33b5c8f98d8b1d6b28f080cda15e1734ce09014e
elswis <151661248+elswis@users.noreply.github.com>
2026-05-21 09:45:04 +0100

    update

--- app/about/component/dist/Value.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ValuesContent_1 = require("@/app/home/ValuesContent");
-var react_1 = require("react");
-var Value = function () {
-    return (react_1["default"].createElement("div", null,
-        react_1["default"].createElement(ValuesContent_1["default"], null)));
-};
-exports["default"] = Value;
